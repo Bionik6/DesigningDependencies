f31db28a0b6ca67b8a80b1f6f7516511cff6425a
Ibrahima Ciss <bionik6@gmail.com>
2021-08-20 19:06:57 +0000

    Initial Commit

---
+++ DesigningDependencies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesigningDependencies
+//
+//  Created by Ibrahima Ciss on 20/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesigningDependencies/DesigningDependenciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesigningDependenciesApp.swift
+//  DesigningDependencies
+//
+//  Created by Ibrahima Ciss on 20/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct DesigningDependenciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
