35bef00def9c4d9264e78f02bf4a51560bc4b5e2
Ibrahima Ciss <bionik6@gmail.com>
2021-08-24 22:20:51 +0000

    Exporting all dependencies to `WeatherClient` package

--- DesigningDependencies/ContentView.swift
+++ DesigningDependencies/ContentView.swift
@@ -1,64 +1,8 @@
 import SwiftUI
 import Combine
 import CoreLocation
+import WeatherClient
 
-struct Location { }
-
-struct WeatherClient {
-  var weather: () -> AnyPublisher<WeatherResponse, Error>
-  var searchLocations: (CLLocationCoordinate2D) -> AnyPublisher<[Location], Error>
-}
-
-extension WeatherClient {
-  static let live = Self(weather: {
-    URLSession.shared.dataTaskPublisher(for: URL(string: "https://www.metaweather.com/api/location/2459115")!)
-      .map { data, _ in data }
-      .decode(type: WeatherResponse.self, decoder: weatherJsonDecoder)
-      .receive(on: DispatchQueue.main)
-      .eraseToAnyPublisher()
-  }, searchLocations:  { coordinates in
-    fatalError()
-  })
-}
-
-
-extension WeatherClient {
-  static let empty = Self(weather: {
-    Just(WeatherResponse(consolidatedWeather: []))
-      .setFailureType(to: Error.self)
-      .eraseToAnyPublisher()
-  }, searchLocations:  { coordinates in
-    Just([])
-      .setFailureType(to: Error.self)
-      .eraseToAnyPublisher()
-  })
-  
-  static let happyPath = Self(weather: {
-    Just(
-      WeatherResponse(
-        consolidatedWeather: [
-          .init(applicableDate: Date(), id: 1, maxTemp: 30, minTemp: 10, theTemp: 20),
-          .init(applicableDate: Date().addingTimeInterval(86400), id: 2, maxTemp: -10, minTemp: -30, theTemp: -20)
-        ]
-      )
-    )
-    .setFailureType(to: Error.self)
-    .eraseToAnyPublisher()
-  }, searchLocations: { _ in
-    Just([])
-      .setFailureType(to: Error.self)
-      .eraseToAnyPublisher()
-  })
-  
-  static let failed = Self(weather: {
-    Fail(error: NSError(domain: "", code: 1))
-      .eraseToAnyPublisher()
-  }, searchLocations: { _ in
-    Just([])
-      .setFailureType(to: Error.self)
-      .eraseToAnyPublisher()
-  })
-}
 
 class AppViewModel: ObservableObject {
   var weatherClient: WeatherClient
@@ -134,26 +78,4 @@ let dayOfWeekFormatter: DateFormatter = {
   let formatter = DateFormatter()
   formatter.dateFormat = "EEEE"
   return formatter
-}()
-
-
-struct WeatherResponse: Decodable, Equatable {
-  var consolidatedWeather: [ConsolidatedWeather]
-  
-  struct ConsolidatedWeather: Decodable, Equatable {
-    var applicableDate: Date
-    var id: Int
-    var maxTemp: Double
-    var minTemp: Double
-    var theTemp: Double
-  }
-}
-
-private let weatherJsonDecoder: JSONDecoder = {
-  let jsonDecoder = JSONDecoder()
-  let formatter = DateFormatter()
-  formatter.dateFormat = "yyyy-MM-dd"
-  jsonDecoder.dateDecodingStrategy = .formatted(formatter)
-  jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-  return jsonDecoder
-}()
+  }()
