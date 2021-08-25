557464d748cb1945c83f52ea613145a7b8d279b8
Ibrahima Ciss <bionik6@gmail.com>
2021-08-25 07:07:10 +0000

    Update the Package.swift file

---
+++ DesigningDependencies/WeatherClient/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "WeatherClient",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(name: "WeatherClient", targets: ["WeatherClient"]),
+    .library(name: "WeatherClientLive", targets: ["WeatherClientLive"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(name: "WeatherClient", dependencies: []),
+    .target(name: "WeatherClientLive", dependencies: ["WeatherClient"]),
+    .testTarget(
+      name: "WeatherClientTests",
+      dependencies: ["WeatherClient"]),
+  ]
+)
