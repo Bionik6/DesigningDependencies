81d6d10988e64a9c38ce30e7242190e9b84c25e4
Ibrahima Ciss <bionik6@gmail.com>
2021-09-02 08:21:30 +0000

    Introducing in `LocationClient` some delegate events

--- DesigningDependencies/WeatherFeature/Sources/WeatherFeature/ContentView.swift
+++ DesigningDependencies/WeatherFeature/Sources/WeatherFeature/ContentView.swift
@@ -4,6 +4,62 @@ import CoreLocation
 import WeatherClient
 import PathMonitorClient
 
+
+struct LocationClient {
+  var requestLocation: () -> Void
+  var requestWhenInUseAuthorization: () -> Void
+  var authorizationStatus: () -> CLAuthorizationStatus
+  // var setDelegate: (CLLocationManagerDelegate) -> ()
+  var delegate: AnyPublisher<DelegateEvent, Never>
+  
+  enum DelegateEvent {
+    case didChangeAuthorizationStatus(CLAuthorizationStatus)
+    case didUpdateLocations([CLLocation])
+    case didFailWithError(Error)
+  }
+}
+
+extension LocationClient {
+  
+  static var live: Self {
+    
+    class Delegate: NSObject, CLLocationManagerDelegate {
+      var subject = PassthroughSubject<DelegateEvent, Never>()
+    
+      init(subject: PassthroughSubject<DelegateEvent, Never>) {
+        self.subject = subject
+      }
+      
+      func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        subject.send(.didChangeAuthorizationStatus(manager.authorizationStatus))
+      }
+      
+      func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        subject.send(.didUpdateLocations(locations))
+      }
+      
+      func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        subject.send(.didFailWithError(error))
+      }
+    }
+    
+    let locationManager = CLLocationManager()
+    let subject = PassthroughSubject<DelegateEvent, Never>()
+    var delegate: Delegate? = Delegate(subject: subject)
+    locationManager.delegate = delegate
+    
+    return Self(
+      requestLocation: locationManager.requestLocation,
+      requestWhenInUseAuthorization: locationManager.requestWhenInUseAuthorization,
+      authorizationStatus: { locationManager.authorizationStatus },
+      delegate: subject
+        .handleEvents(receiveCancel: { delegate = nil })
+        .eraseToAnyPublisher()
+    )
+  }
+  
+}
+
 public class AppViewModel: NSObject, ObservableObject {
   private let weatherClient: WeatherClient
   private let locationManager: CLLocationManager
