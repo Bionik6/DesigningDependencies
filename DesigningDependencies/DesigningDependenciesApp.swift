d27b9065af2fe950f0960d6fd292f4758a445dd1
Ibrahima Ciss <bionik6@gmail.com>
2021-09-02 07:10:16 +0000

    Fix errors in DesigningDependenciesApp

--- DesigningDependencies/DesigningDependenciesApp.swift
+++ DesigningDependencies/DesigningDependenciesApp.swift
@@ -1,13 +1,13 @@
 import SwiftUI
 import WeatherFeature
 import WeatherClientLive
-import PathMonitorClient
+import PathMonitorClientLive
 
 @main
 struct DesigningDependenciesApp: App {
   var body: some Scene {
     WindowGroup {
-      ContentView(viewModel: AppViewModel(weatherClient: .live, pathMonitorClient: PathMonitorClient.live(queue: .main)))
+      ContentView(viewModel: AppViewModel(weatherClient: .live, pathMonitorClient: .live(queue: .main)))
     }
   }
 }
